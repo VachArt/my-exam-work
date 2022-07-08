3ff58cb4cf8519a0f0f22d5cbd0ec68b7532dd3e
VachArt <temster92@gmail.com>
2022-07-08 10:48:12 +0600

    first commit

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header_info'>
+                <h1 className='header_title'>How the scenes fit together</h1>
+                <p className='header_text'>Employee Training & Development, Diversity & Inclusion Programs, and Conflict Resolution.</p>
+            </div>
+            <div className='header_image'>
+                
+            </div>
+        </div>
+    )
+}
